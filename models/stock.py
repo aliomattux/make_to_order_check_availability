879289af3191c3b3abc8fb0f410bceabb2c11b22
aliomattux <kyle.waid@gcotech.com>
2015-01-05 16:01:03 -0600

    First Commit

---
+++ models/stock.py
@@ -0,0 +1,13 @@
+from openerp.osv import osv, fields
+
+
+class StockMove(osv.osv):
+    _inherit = 'stock.move'
+
+
+    def action_confirm(self, cr, uid, ids, context=None):
+        for move in self.browse(cr, uid, ids, context=context):
+	    if move.procure_method == 'make_to_order' and round(move.availability, 2) >= round(move.product_qty, 2):
+	        move.procure_method = 'make_to_stock'
+
+	return super(StockMove, self).action_confirm(cr, uid, ids, context)
