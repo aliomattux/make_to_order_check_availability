750cb7fc0cf77824b18dc08301c7e6da69656c80
Kyle Waid <kyle.waid@gcotech.com>
2016-05-13 16:28:32 -0400

    Add make to order phantom fix

--- models/stock.py
+++ models/stock.py
@@ -7,7 +7,9 @@ class StockMove(osv.osv):
 
     def action_confirm(self, cr, uid, ids, context=None):
         for move in self.browse(cr, uid, ids, context=context):
-	    if move.procure_method == 'make_to_order' and round(move.availability, 2) >= round(move.product_qty, 2):
+	    if move.procurement_id.name == 'Phantom BOM':
+		move.procure_method = 'make_to_stock'
+	    elif move.procure_method == 'make_to_order' and round(move.availability, 2) >= round(move.product_qty, 2):
 	        move.procure_method = 'make_to_stock'
 
 	return super(StockMove, self).action_confirm(cr, uid, ids, context)
